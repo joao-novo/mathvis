f9a754609374302b8b38b448263ae144a8ec3b1d
João Novo <joaonovo05@protonmail.com>
2025-01-29 14:29:52 +0000

    feat: 2d point implementation

--- src/api/point.rs
+++ src/api/point.rs
@@ -1,20 +1,18 @@
-use std::ops::Add;
-
-use super::{motion::Move, screen::Screen, vector::Vector2D};
+use super::{motion::Move, screen::Screen2D, vector::Vector2D};
 
 pub trait Point2DLike<'a> {
-    fn is_within_context(context: &'a Screen, x: f64, y: f64) -> bool;
-    fn new(context: &'a Screen, x: f64, y: f64) -> Option<Self>
+    fn is_within_context(context: &'a Screen2D, x: f64, y: f64) -> bool;
+    fn new(context: &'a Screen2D, x: f64, y: f64) -> Option<Self>
     where
         Self: Sized;
-    fn origin(context: &'a Screen) -> Self;
+    fn origin(context: &'a Screen2D) -> Self;
     fn x(&self) -> f64;
     fn y(&self) -> f64;
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Point2D<'a> {
-    context: &'a Screen,
+    context: &'a Screen2D,
     x: f64,
     y: f64,
 }
@@ -26,7 +24,7 @@ impl<'a> Point2D<'a> {
         (dx * dx + dy * dy).sqrt()
     }
 
-    fn add_vector(&self, vec: Vector2D) -> Result<Self, &str>
+    pub fn add_vector(&self, vec: Vector2D) -> Result<Self, &str>
     where
         Self: Sized,
     {
@@ -38,18 +36,18 @@ impl<'a> Point2D<'a> {
 }
 
 impl<'a> Point2DLike<'a> for Point2D<'a> {
-    fn is_within_context(context: &'a Screen, x: f64, y: f64) -> bool {
+    fn is_within_context(context: &'a Screen2D, x: f64, y: f64) -> bool {
         x >= 0. && x < context.width() as f64 && y >= 0. && y < context.height() as f64
     }
 
-    fn new(context: &'a Screen, x: f64, y: f64) -> Option<Point2D<'a>> {
+    fn new(context: &'a Screen2D, x: f64, y: f64) -> Option<Point2D<'a>> {
         if Self::is_within_context(context, x, y) {
             return Some(Point2D { context, x, y });
         }
         None
     }
 
-    fn origin(context: &'a Screen) -> Self {
+    fn origin(context: &'a Screen2D) -> Self {
         Self::new(context, 0.0, 0.0).unwrap()
     }
 
