f96bddeda2e172506ef6ecffbc298706e3bfa3f4
João Novo <joaonovo05@protonmail.com>
2025-01-29 14:29:02 +0000

    feat: implemented 3d screen

--- rust/src/api/screen.rs
+++ rust/src/api/screen.rs
@@ -1,22 +1,86 @@
-#[derive(Debug, PartialEq, Clone, Copy)]
-pub struct Screen {
-    width: u32,
-    height: u32,
+use std::ops::Range;
+
+pub trait Screen2DLike {
+    fn x_axis(&self) -> &Range<i32>;
+    fn y_axis(&self) -> &Range<i32>;
 }
 
-impl Screen {
-    pub fn new(width: u32, height: u32) -> Option<Self> {
-        if width > 0 && height > 0 {
-            return Some(Screen { width, height });
+#[derive(Debug, PartialEq, Clone)]
+pub struct Screen2D {
+    x_axis: Range<i32>,
+    y_axis: Range<i32>,
+}
+
+pub struct Screen3D {
+    x_axis: Range<i32>,
+    y_axis: Range<i32>,
+    z_axis: Range<i32>,
+}
+
+impl Screen2D {
+    pub fn new((xstart, xend): (i32, i32), (ystart, yend): (i32, i32)) -> Option<Self> {
+        if xstart < xend && ystart < yend {
+            return Some(Screen2D {
+                x_axis: Range {
+                    start: xstart,
+                    end: xend,
+                },
+                y_axis: Range {
+                    start: ystart,
+                    end: yend,
+                },
+            });
+        }
+        None
+    }
+}
+
+impl Screen2DLike for Screen2D {
+    fn x_axis(&self) -> &Range<i32> {
+        &self.x_axis
+    }
+
+    fn y_axis(&self) -> &Range<i32> {
+        &self.y_axis
+    }
+}
+
+impl Screen3D {
+    pub fn new(
+        (xstart, xend): (i32, i32),
+        (ystart, yend): (i32, i32),
+        (zstart, zend): (i32, i32),
+    ) -> Option<Self> {
+        if xstart < xend && ystart < yend {
+            return Some(Screen3D {
+                x_axis: Range {
+                    start: xstart,
+                    end: xend,
+                },
+                y_axis: Range {
+                    start: ystart,
+                    end: yend,
+                },
+                z_axis: Range {
+                    start: zstart,
+                    end: zend,
+                },
+            });
         }
         None
     }
 
-    pub fn width(&self) -> u32 {
-        self.width
+    pub fn z_axis(&self) -> &Range<i32> {
+        &self.z_axis
+    }
+}
+
+impl Screen2DLike for Screen3D {
+    fn x_axis(&self) -> &Range<i32> {
+        &self.x_axis
     }
 
-    pub fn height(&self) -> u32 {
-        self.height
+    fn y_axis(&self) -> &Range<i32> {
+        &self.y_axis
     }
 }
