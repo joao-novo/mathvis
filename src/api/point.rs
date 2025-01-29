f5d6711e9c86d65e4e7a8635cf590e4170db20e7
João Novo <joaonovo05@protonmail.com>
2025-01-29 13:56:24 +0000

    feat: created basic 2d point struct and trait for working with similar structures

--- src/api/point.rs
+++ src/api/point.rs
@@ -1,16 +1,75 @@
-pub struct Point2D(f64, f64);
+use std::ops::Add;
 
-impl Point2D {
-    #[inline]
-    pub fn new(x: f64, y: f64) -> Point2D {
-        Point2D(x, y)
+use super::{motion::Move, screen::Screen, vector::Vector2D};
+
+pub trait Point2DLike<'a> {
+    fn is_within_context(context: &'a Screen, x: f64, y: f64) -> bool;
+    fn new(context: &'a Screen, x: f64, y: f64) -> Option<Self>
+    where
+        Self: Sized;
+    fn origin(context: &'a Screen) -> Self;
+    fn x(&self) -> f64;
+    fn y(&self) -> f64;
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Point2D<'a> {
+    context: &'a Screen,
+    x: f64,
+    y: f64,
+}
+
+impl<'a> Point2D<'a> {
+    pub fn distance_to(&self, other: &Point2D) -> f64 {
+        let dx = self.x - other.x;
+        let dy = self.y - other.y;
+        (dx * dx + dy * dy).sqrt()
     }
 
-    pub fn x(&self) -> f64 {
-        self.0
+    fn add_vector(&self, vec: Vector2D) -> Result<Self, &str>
+    where
+        Self: Sized,
+    {
+        if let Some(point) = Self::new(self.context, self.x + vec.x(), self.y + vec.y()) {
+            return Ok(point);
+        }
+        Err("out of bounds")
+    }
+}
+
+impl<'a> Point2DLike<'a> for Point2D<'a> {
+    fn is_within_context(context: &'a Screen, x: f64, y: f64) -> bool {
+        x >= 0. && x < context.width() as f64 && y >= 0. && y < context.height() as f64
     }
 
-    pub fn y(&self) -> f64 {
-        self.1
+    fn new(context: &'a Screen, x: f64, y: f64) -> Option<Point2D<'a>> {
+        if Self::is_within_context(context, x, y) {
+            return Some(Point2D { context, x, y });
+        }
+        None
+    }
+
+    fn origin(context: &'a Screen) -> Self {
+        Self::new(context, 0.0, 0.0).unwrap()
+    }
+
+    fn x(&self) -> f64 {
+        self.x
+    }
+
+    fn y(&self) -> f64 {
+        self.y
+    }
+}
+
+impl<'a> Move for Point2D<'a> {
+    fn move_to(&self, x: f64, y: f64) -> Result<Self, &str>
+    where
+        Self: Sized,
+    {
+        if let Some(point) = Self::new(self.context, x, y) {
+            return Ok(point);
+        }
+        Err("out of bounds")
     }
 }
